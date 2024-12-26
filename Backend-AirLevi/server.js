81ddd6538f3212017bae161070accffcd912486d
Refael Levi <refael14levi@gmail.com>
2024-12-27 01:41:08 +0200

    update port

--- Backend-AirLevi/server.js
+++ Backend-AirLevi/server.js
@@ -76,7 +76,7 @@ app.get('/**', (req, res) => {
 
 
 const logger = require('./services/logger.service')
-const port = process.env.PORT || 3030
-http.listen(port, () => {
+const PORT = process.env.PORT || 3030
+http.listen(PORT, () => {
     logger.info('Server is running on port: ' + port)
 })
