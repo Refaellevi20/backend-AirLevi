4e89122858956e2106dc675ef33034ea7fd0f66a
Refael Levi <refael14levi@gmail.com>
2024-12-27 02:58:11 +0200

    change to server

--- Backend-AirLevi/server.js
+++ Backend-AirLevi/server.js
@@ -3,16 +3,17 @@ const cors = require('cors')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const axios = require('axios')
+require('dotenv').config()
 
 const app = express()
-const http = require('http').createServer(app)
+const server = require('http').createServer(app)
 
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'))
 
-// setupSocketAPI(server)
+
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
@@ -37,28 +38,6 @@ const orderRoutes = require('./api/order/order.routes')
 const stayRoutes = require('./api/stay/stay.routes')
 const {setupSocketAPI} = require('./services/socket.service')
 
-// app.post('/ask-ai', async (req, res) => {
-//     const question = req.body.question;
-//     if (!question) {
-//         return res.status(400).send({ error: 'Question is required' })
-//     }
-
-//     try {
-//         const response = await axios.post('https://api.openai.com/v1/completions', {
-//             model: 'text-davinci-003',
-//             prompt: question,
-//             max_tokens: 150
-//         }, {
-//             headers: {
-//                 'Authorization': `Bearer YOUR_OPENAI_API_KEY`
-//             }
-//         })
-//         res.json({ answer: response.data.choices[0].text.trim() })
-//     } catch (error) {
-//         console.error('Error:', error);
-//         res.status(500).json({ error: 'Failed to get an answer from AI' })
-//     }
-// })
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
@@ -68,7 +47,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/stay', stayRoutes)
-setupSocketAPI(http)
+setupSocketAPI(server)
 
 app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -77,6 +56,6 @@ app.get('/**', (req, res) => {
 
 const logger = require('./services/logger.service')
 const PORT = process.env.PORT || 3030
-http.listen(PORT, () => {
-    logger.info('Server is running on port: ' + port)
+server.listen(PORT, () => {
+    logger.info(`Server running at http://localhost:${PORT}`)
 })
