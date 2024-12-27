427b7d29b5c547846fbf167ebd9ecc11ff886f63
Refael Levi <refael14levi@gmail.com>
2024-12-27 14:05:17 +0200

    another test

--- Backend-AirLevi/server.js
+++ Backend-AirLevi/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(express.static(path.join(__dirname, 'public')))
 } else {
     const corsOptions = {
         origin: [
