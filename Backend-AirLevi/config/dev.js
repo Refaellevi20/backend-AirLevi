d28330894bc4e9be55172684c436c38901ccf77d
Refael Levi <refael14levi@gmail.com>
2024-12-28 18:22:38 +0200

    a test

--- Backend-AirLevi/config/dev.js
+++ Backend-AirLevi/config/dev.js
@@ -1,7 +1,22 @@
-module.exports = {
-  dbURL: process.env.DB_URL || 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
- dbName:'AirLevi_db',
-}
+// module.exports = {
+//   dbURL: process.env.DB_URL || 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+//  dbName:'AirLevi_db',
+// }
+
+require('dotenv').config();
+
+const config = {
+    dbURL: process.env.MONGO_DB_URL || 'mongodb+srv://Raf_Levi:Raf_levi123@cluster0.rkdpf.mongodb.net/AirLevi_db?retryWrites=true&w=majority',
+    dbName: 'AirLevi_db',
+    isGuestMode: true,
+};
+
+const mongoose = require('mongoose');
+mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+
 
 // module.exports = {
 //   dbURL: 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
