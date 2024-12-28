104f8349430b3d33cfd9141cbd519d90e8e10b78
Refael Levi <refael14levi@gmail.com>
2024-12-28 23:52:11 +0200

    a test

--- Backend-AirLevi/config/dev.js
+++ Backend-AirLevi/config/dev.js
@@ -1,7 +1,12 @@
+// module.exports = {
+//   dbURL: 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+//  dbName: 'AirLevi_db',
+//  "isGuestMode":true
+// }
+
 module.exports = {
-  dbURL: 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
- dbName: 'AirLevi_db',
- "isGuestMode":true
+dbURL: 'mongodb+srv://Raf_Levi:Raf_Levi123@cluster0.rkdpf.mongodb.net/AirLevi_db?retryWrites=true&w=majority',
+dbName: 'AirLevi_db'
 }
 
 // require('dotenv').config();
