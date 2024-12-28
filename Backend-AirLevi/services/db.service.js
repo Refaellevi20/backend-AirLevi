e9243ff81ad1ce0c41e68fecdff23ddff2c37f48
Refael Levi <refael14levi@gmail.com>
2024-12-28 18:50:12 +0200

    a test

--- Backend-AirLevi/services/db.service.js
+++ Backend-AirLevi/services/db.service.js
@@ -2,43 +2,42 @@ const MongoClient = require('mongodb').MongoClient
 const config = require('../config')
 const logger = require('./logger.service')
 
-// module.exports = {
-//     getCollection
-// }
-
 const dbService = {
     getCollection
-}
+};
 
 module.exports = dbService
-
 
 var dbConn = null
 
 async function getCollection(collectionName) {
     try {
-        const db = await connect()
+        const db = await _connect()
+
         const collection = await db.collection(collectionName)
+        
+
         return collection
     } catch (err) {
+        console.log('had troubels connectiong on get');
+
         logger.error('Failed to get Mongo collection', err)
         throw err
     }
 }
 
-async function connect() {
+async function _connect() {
     if (dbConn) return dbConn
+
     try {
-        console.log('config',config)
-        console.log('Attempting to connect to the database...')
+        console.log(config)
         const client = await MongoClient.connect(config.dbURL)
-        const db = client.db(config.dbName)
-        dbConn = db
-        console.log('Connected to the database')
-        return db
+        return dbConn = client.db(config.dbName)
     } catch (err) {
         logger.error('Cannot Connect to DB', err)
         throw err
     }
 }
+
+
 
