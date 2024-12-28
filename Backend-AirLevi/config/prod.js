c0919ea6a274cd74c4c9df04f2149ba698e7f407
Refael Levi <refael14levi@gmail.com>
2024-12-28 18:29:08 +0200

    a test

--- Backend-AirLevi/config/prod.js
+++ Backend-AirLevi/config/prod.js
@@ -1,15 +1,21 @@
-require('dotenv').config();
+module.exports = {
+  dbURL: 'mongodb+srv://Raf_Levi:Raf_levi123@cluster0.rkdpf.mongodb.net/AirLevi_db?retryWrites=true&w=majority',
+  dbName: 'AirLevi_db'
+}
 
-const config = {
-    dbURL: process.env.MONGO_DB_URL || 'mongodb+srv://Raf_Levi:Raf_levi123@cluster0.rkdpf.mongodb.net/AirLevi_db?retryWrites=true&w=majority',
-    dbName: 'AirLevi_db',
-    isGuestMode: true,
-};
 
-const mongoose = require('mongoose');
-mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB successfully'))
-    .catch((err) => console.error('Failed to connect to MongoDB', err))
+// require('dotenv').config();
+
+// const config = {
+//     dbURL: process.env.MONGO_DB_URL || 'mongodb+srv://Raf_Levi:Raf_levi123@cluster0.rkdpf.mongodb.net/AirLevi_db?retryWrites=true&w=majority',
+//     dbName: 'AirLevi_db',
+//     isGuestMode: true,
+// };
+
+// const mongoose = require('mongoose');
+// mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB successfully'))
+//     .catch((err) => console.error('Failed to connect to MongoDB', err))
 
 // const mongoose = require('mongoose');
 
